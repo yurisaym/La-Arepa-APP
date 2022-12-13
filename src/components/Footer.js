2ba1df94b35fdbfa9693136530870515ac9d40bb
yerly <ydomana@gmail.com>
2022-12-12 23:04:05 -0300

    modificaçcao footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,12 @@ import React  from "react";
 
 function Footer() {
     return (
-        <div>
+        <div className="rodape">
             <ul class="socialMedia">
                 <i id="social" class="fab fa-instagram"></i>
-                <i  id="social" class="fab fa-linkedin"></i>
-                <i  id="social" class="fab fa-twitter-square"></i>
-                <i   id= "social" ></i>
+                <i id="social" class="fab fa-linkedin"></i>
+                <i id="social" class="fab fa-twitter-square"></i>
+                <i id= "social" class="fab fa-facebook-square"></i>
             </ul>
         </div>
        
